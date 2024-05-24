241610a5ccc509a00feee5b5a8241ac2ec6fbbe0
Chicco <frcpalma@gmail.com>
2024-05-24 19:57:37 +0200

    compatibility with core v 1.6

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ AI's personality - here you can define a prompt phrase that may give it a proper
 AI's language - here you can define the language you want to use for the conversation. By selecting 'AI detection', the AI will always answer in the same language in which the query was asked.
 
 Tags and prompts - here you can create up to 3 custom tags, each linked to a unique prompt. When initiating a conversation in the chat, use the @tag to prompt the AI to respond accordingly, enhancing the relevance and specificity of its answers.
+
+## Release note
+Smooth Talk plugin 3.0.0 works only with Cheshire Cat AI 1.6.0 and above.
--- smooth_talk.py
+++ smooth_talk.py
@@ -15,41 +15,41 @@ def before_cat_reads_message(user_message_json: Dict, cat) -> Dict:
     tag2 = "@" + settings["tag_2"]
     tag3 = "@" + settings["tag_3"]
 
-    query = user_message_json["text"]
+    query = user_message_json.text
     try:
         tag = re.search(r"^(@?\w+)", query).group(1)
     except Exception:
         tag = "|_|"
 
     if tag == tag1:
-        cat.working_memory.update({"tag": 1})
-        user_message_json["text"] = re.sub(r"^@(\w+)\s+", "", user_message_json["text"])
+        cat.working_memory.tag = 1
+        user_message_json.text = re.sub(r"^@(\w+)\s+", "", user_message_json.text)
     elif tag == tag2:
-        cat.working_memory.update({"tag": 2})
-        user_message_json["text"] = re.sub(r"^@(\w+)\s+", "", user_message_json["text"])
+        cat.working_memory.tag = 2
+        user_message_json.text = re.sub(r"^@(\w+)\s+", "", user_message_json.text)
     elif tag == tag3:
-        cat.working_memory.update({"tag": 3})
-        user_message_json["text"] = re.sub(r"^@(\w+)\s+", "", user_message_json["text"])
+        cat.working_memory.tag = 3
+        user_message_json.text = re.sub(r"^@(\w+)\s+", "", user_message_json.text)
     else:
-        cat.working_memory.update({"tag": 0})
+        cat.working_memory.tag = 0
 
-    log.info(f"Detected tag: {cat.working_memory['tag']}")
-    log.info(f"User message: {user_message_json['text']}")
+    log.info(f"Detected tag: {cat.working_memory.tag}")
+    log.info(f"User message: {user_message_json.text}")
 
     # Detection user's message language
 
     language_selected = settings["language"]
 
     if language_selected == "AI detection":
-        prompt_language = f"""Accurately determine the language of this given human message: '{user_message_json['text']}'.
-        Output the results only in the standardized JSON format, with the structure: {{"language":"detected_language"}} - example {{"language":"English"}}."""
+        prompt_language = f"""Accurately determine the language of this given human message: '{user_message_json.text}'.
+        Output the results only with the structure: {{"language":"detected_language"}} - example: {{"language":"English"}}."""
         language = cat.llm(prompt=prompt_language)
 
         try:
-            cat.working_memory.update(json.loads(language))
-            log.info(f"Detected language: {cat.working_memory['language']}")
+            cat.working_memory.language = json.loads(language)["language"]
+            log.info(f"Detected language: {cat.working_memory.language}")
         except json.JSONDecodeError:
-            cat.working_memory.update({"language": "English"})
+            cat.working_memory.language = "English"
             log.error(f"Error: Unable to detect language {language}")
 
     else:
@@ -69,19 +69,22 @@ def agent_prompt_prefix(prefix: str, cat) -> str:
     prompt2 = settings["prompt_2"]
     prompt3 = settings["prompt_3"]
 
-    if cat.working_memory["tag"] == 1:
+    if cat.working_memory.tag == 1:
         prefix = f"""{prompt1}
-        Answer the human with a focus on the following Context in {cat.working_memory['language']} language."""
-    if cat.working_memory["tag"] == 2:
+        Answer the human with a focus on the following Context in {cat.working_memory.language} language."""
+
+    if cat.working_memory.tag == 2:
         prefix = f"""{prompt2}
-        Answer the human with a focus on the following Context in {cat.working_memory['language']} language."""
-    if cat.working_memory["tag"] == 3:
+        Answer the human with a focus on the following Context in {cat.working_memory.language} language."""
+
+    if cat.working_memory.tag == 3:
         prefix = f"""{prompt3}
-        Answer the human with a focus on the following Context in {cat.working_memory['language']} language."""
-    if cat.working_memory["tag"] == 0:
+        Answer the human with a focus on the following Context in {cat.working_memory.language} language."""
+
+    if cat.working_memory.tag == 0:
         prefix = f"""{personality}
         Keep a balanced and smooth conversation with the human.
         If you need clarification to answer satisfactorily, ask a question to the human.
-        Answer the human with a focus on the following Context in {cat.working_memory['language']} language."""
+        Answer the human with a focus on the following Context in {cat.working_memory.language} language."""
 
     return prefix
